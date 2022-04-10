4610b70fcb66b6c3beb5510d1a60e3931e8a4882
Leonardo Nakahara <leonardo.nakahara@gmail.com>
2022-04-10 20:51:54 -0300

    Initial Commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0xD90eF9Be1c4db699Fd2EF415629535b389748Ac9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
